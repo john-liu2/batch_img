c8d54f793894dac39b92df31a0497205d297539b
John Liu <johnliu2@gmail.com>
2025-08-19 13:23:28 -0700

    Update

--- tests/test_common.py
+++ tests/test_common.py
@@ -16,7 +16,7 @@ from batch_img.const import NAME, PKG_NAME, VER
 from .helper import DotDict
 
 
-@pytest.fixture(params=[(PKG_NAME, "0.0.9"), ("", "0.0.9")])
+@pytest.fixture(params=[(PKG_NAME, "0.0.9"), ("", "0.1.0")])
 def ver_data(request):
     return request.param
 
